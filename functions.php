8ce618197994dc2cc82d25d04479632a71523587
ninaaa12334 <160941480+ninaaa12334@users.noreply.github.com>
2025-04-15 17:42:46 +0200

    ..

--- functions.php
+++ functions.php
@@ -1,52 +1,19 @@
-<?php 
-function job_board_enqueue_scripts() {
+<?php
+function jobboard_scripts() {
     wp_enqueue_style('bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css');
-    wp_enqueue_script('bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js', array('jquery'), '', true);
-    wp_enqueue_script('custom-js', get_template_directory_uri() . '/js/script.js', array('jquery'), '', true);
+    wp_enqueue_style('jobboard-style', get_stylesheet_uri());
+
+    wp_enqueue_script('bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js', array('jquery'), null, true);
+    wp_enqueue_script('jobboard-scripts', get_template_directory_uri() . '/js/scripts.js', array('jquery'), null, true);
 }
-add_action('wp_enqueue_scripts', 'job_board_enqueue_scripts');
+add_action('wp_enqueue_scripts', 'jobboard_scripts');
 
-
-function job_board_custom_post_types() {
-    register_post_type('job', array(
-        'labels' => array('name' => __('Jobs'), 'singular_name' => __('Job')),
-        'public' => true,
-        'has_archive' => true,
-        'supports' => array('title', 'editor', 'thumbnail', 'custom-fields'),
-        'menu_icon' => 'dashicons-briefcase',
-    ));
-
-    register_post_type('application', array(
-        'labels' => array('name' => __('Applications'), 'singular_name' => __('Application')),
-        'public' => true,
-        'has_archive' => true,
-        'supports' => array('title', 'editor', 'custom-fields'),
-        'menu_icon' => 'dashicons-id',
+function jobboard_menus() {
+    register_nav_menus(array(
+        'main-menu' => __('Main Menu')
     ));
 }
-add_action('init', 'job_board_custom_post_types');
+add_action('init', 'jobboard_menus');
 
-
-
-function handle_application_submission() {
-    if (!empty($_POST['applicant_name']) && !empty($_POST['applicant_email']) && !empty($_POST['cover_letter'])) {
-        $new_post = array(
-            'post_title'   => sanitize_text_field($_POST['applicant_name']),
-            'post_content' => sanitize_textarea_field($_POST['cover_letter']),
-            'post_status'  => 'pending',
-            'post_type'    => 'application',
-            'meta_input'   => array(
-                'job_id'   => intval($_POST['job_id']),
-                'email'    => sanitize_email($_POST['applicant_email'])
-            )
-        );
-        wp_insert_post($new_post);
-        wp_redirect(home_url('/thank-you/'));
-        exit;
-    }
-}
-add_action('admin_post_submit_application', 'handle_application_submission');
-add_action('admin_post_nopriv_submit_application', 'handle_application_submission');
-
-?>
+require get_template_directory() . '/inc/custom-post-types.php';
 
